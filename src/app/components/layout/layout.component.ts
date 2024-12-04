0d6af7988ceb98c102ebad9737e22fa2540ef62f
Kendrick <kendrick.oppong@amalitech.com>
2024-12-04 15:05:51 +0000

    feat: add dynamic layout for routing

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { SideNavComponent } from "./side-nav/side-nav.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, SideNavComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
