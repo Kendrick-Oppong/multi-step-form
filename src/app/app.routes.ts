f2783e3950483e45f9979d4ba3df461bee332e25
Kendrick <kendrick.oppong@amalitech.com>
2024-12-04 07:19:10 +0300

    chore: setup route and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { Step1Component } from './components/step-1/step-1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: 'step1', component: Step1Component }],
+  },
+  { path: '**', redirectTo: '' },
+];
