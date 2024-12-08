fb0fc7c330aae2612aa607711c5dc3ee3470429e
Kendrick <kendrick.oppong@amalitech.com>
2024-12-09 01:23:50 +0300

    chore: setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { Step1Component } from './pages/step-1/step-1.component';
-import { Step2Component } from './components/step-2/step-2.component';
-import { Step3Component } from './components/step-3/step-3.component';
-import { Step4Component } from './components/step-4/step-4.component';
+import { Step2Component } from '@pages/step-2/step-2.component';
+import { Step3Component } from '@pages/step-3/step-3.component';
+import { Step4Component } from '@pages/step-4/step-4.component';
+
 
 export const routes: Routes = [
   {
