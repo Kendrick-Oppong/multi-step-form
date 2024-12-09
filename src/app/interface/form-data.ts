4b82997a83323ba23321fc778b2b5eefe744184a
Kendrick <kendrick.oppong@amalitech.com>
2024-12-09 07:35:29 +0000

    feat: implement step 4 route

--- src/app/interface/form-data.ts
+++ src/app/interface/form-data.ts
@@ -1,4 +1,7 @@
-export interface FormData {}
+import { step2Config, step3Config } from '@constants/form-config.constants';
+
+export type StepTwoConfig = (typeof step2Config)[number];
+export type StepThreeConfig = (typeof step3Config)[number];
 
 export enum STORAGE_KEY {
   STEP1 = 'step1',
